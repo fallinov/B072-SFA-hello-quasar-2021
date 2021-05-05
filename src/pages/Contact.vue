27a6118f437a5723a2fe8c92994d5bf790cc21ef
Steve Fallet <steve.fallet@divtec.ch>
2021-05-05 10:52:43 +0200

    Initial commit

---
+++ src/pages/Contact.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page >
+    <h1>Page de contact</h1>
+    <p>J'ai {{ age }} ans</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageContact',
+  data () {
+    return {
+      age: 33
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+h1
+  color: $secondary
+</style>
